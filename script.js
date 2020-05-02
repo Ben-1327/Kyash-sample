01dec34da97cf26a3b85e20d548674316195698e
Ben-1327 <62165734+Ben-1327@users.noreply.github.com>
2020-05-02 17:39:43 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.what-kyashvisa').click(function(){
+    $('.contents-list-kyashvisa').slideDown().fadeIn();
+  });
+
+  $('.back-icon').click(function(){
+    $('.contents-list-kyashvisa').slideUp().fadeOut();
+  });
+});
